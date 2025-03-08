090577ab1c266af3f1aafeb11f4e6d16fa2c3d4c
German <yageraaa@gmail.com>
2025-03-08 22:36:33 +0300

    Update __init__.py (ResidualConnection)

--- model/modules/__init__.py
+++ model/modules/__init__.py
@@ -3,3 +3,4 @@ from .positional_encoding import PositionalEncoding
 from .embeddings import InputEmbeddings
 from .feed_forward import FeedForwardLayer
 from .multihead_attention import MultiHeadAttention
+from .residual_connection import ResidualConnection
