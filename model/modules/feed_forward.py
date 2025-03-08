ef3e9cc4406162839790ce23e474eb8dd18098ce
German <yageraaa@gmail.com>
2025-03-08 19:22:55 +0300

    Add feed_forward.py

---
+++ model/modules/feed_forward.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class FeedForwardLayer(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, dropout: float):
+        super().__init__()
+        self.linear1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.linear2(self.dropout(F.gelu(self.linear1(x))))
