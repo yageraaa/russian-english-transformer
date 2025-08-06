ce6c3b064b2a3c4f5b7411bbfeeb89745a2179b3
gera <yageraaa@gmail.com>
2025-08-06 11:38:23 +0300

    fix: change entity to wandb initialization

--- models/training/train.py
+++ models/training/train.py
@@ -24,7 +24,7 @@ def train_model(cfg: DictConfig):
 
     wandb.init(
         project="transformer-ru-en",
-        entity="yageraaa-mtuci",
+        entity="yageraaa-mtuci-org",
         #id=cfg.logging.run_id,
         #resume=cfg.logging.resume,
         config=hydra.utils.instantiate(cfg)
