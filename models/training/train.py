0e2a74bddc7b4b9bac8a57460047b1337f23b070
gera <yageraaa@gmail.com>
2025-09-29 16:15:55 +0300

    fix: suppress noisy logs and warning

--- models/training/train.py
+++ models/training/train.py
@@ -1,3 +1,4 @@
+import warnings
 import logging
 import mlop
 import torch
@@ -19,16 +20,16 @@ import gc
 import re
 import random
 import numpy as np
-import warnings
 
 
 @hydra.main(config_path="../../models/configs", config_name="config", version_base="1.2")
 def train_model(cfg: DictConfig):
+    warnings.filterwarnings("ignore", category=FutureWarning, module="torch.cuda.*")
+    logging.getLogger("pynvml").setLevel(logging.WARNING)
+    logging.getLogger("httpx").setLevel(logging.WARNING)
+    logging.getLogger("mlop").setLevel(logging.WARNING)
+
     accelerator = Accelerator(
-        warnings.filterwarnings("ignore", category=FutureWarning, module="torch.cuda.*"),
-        logging.getLogger("pynvml").setLevel(logging.WARNING),
-        logging.getLogger("httpx").setLevel(logging.WARNING),
-        logging.getLogger("mlop").setLevel(logging.WARNING),
         mixed_precision=getattr(cfg.training, 'mixed_precision', 'no'),
         gradient_accumulation_steps=getattr(cfg.training, 'gradient_accumulation_steps', 1)
     )
